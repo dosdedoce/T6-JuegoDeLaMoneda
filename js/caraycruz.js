ef6c47ced7d6e5b9f80191116368597e34e53f08
dosdedoce <dosdedoce@gmail.com>
2023-06-08 23:26:42 +0200

    Version Final

--- js/caraycruz.js
+++ js/caraycruz.js
@@ -1,16 +1,14 @@
-var tiradaCara=0; 
-var tiradaCruz=0;
-var tiradaGlobal=0;
+let tiradaCara=0; 
+let tiradaCruz=0;
+let tiradaGlobal=0;
 
 let resultadosCaras=document.querySelector('.resultados__cara');
 let resultadosCruces=document.querySelector('.resultados__cruz');
 let btnLanzar=document.getElementById('btnLanzar');
-let btnReiniciar=document.getElementById('btnReiniciar');
-let cara=document.querySelector('.moneda__cara');
-let cruz=document.querySelector('.moneda__cruz');
+let btnReiniciar=document.getElementById('btnReiniciar');;
 let monedas=document.querySelector('.contiene__monedas');
 
-iniciarVbles= () => {
+const iniciarVbles = () => {
         tiradaCara=0; 
         tiradaCruz=0;
         tiradaGlobal=0
@@ -18,55 +16,54 @@ iniciarVbles= () => {
         resultadosCruces.textContent=0;
 };
 
-generarTirada=() =>{
-        let tiradaGlobal=Math.floor(Math.random() * (2));
-        return tiradaGlobal;
-};
-
-contarTirada=()=>{
-        let tirada=generarTirada();
-        if (tirada==0){
-                tiradaCara++;
-        }else{
-                tiradaCruz++;
-        }
-        mostrarTirada(tiradaCara,tiradaCruz);
+const generarTirada=() =>{
+        return Math.floor(Math.random() * (2));
 };
 
 btnLanzar.addEventListener('click',()=>{
         let i=generarTirada();
         monedas.style.animation ='none';
+        monedas.style.opacity=0;
         if(i) {
                 setTimeout(function(){
+                        monedas.style.opacity=1;
                         monedas.style.animation = 'girar-cara 3s forwards'; 
                 }, 100);
                 tiradaCara++;
         }else{
                 setTimeout(function(){
+                        monedas.style.opacity=1;
                         monedas.style.animation = 'girar-cruz 3s forwards';
                 }, 100);
                 tiradaCruz++;
         }
+        monedas.style.opacity=0;
         setTimeout(mostrarTirada, 3000);
         desactivaBoton();
 });
 
-mostrarTirada=()=>{
+const mostrarTirada=()=>{
         resultadosCaras.textContent=tiradaCara;
         resultadosCruces.textContent=tiradaCruz;
 };
 
-
-function desactivaBoton(){
+const desactivaBoton=()=>{
         btnLanzar.disabled = true;
+        btnReiniciar.disabled = true;
+        let colorStyleLanzar=btnLanzar.style.backgroundColor;
+        let colorStyleReiniciar=btnReiniciar.style.backgroundColor;
+        btnLanzar.style.backgroundColor='rgb(222, 222, 222)';
+        btnReiniciar.style.backgroundColor='rgb(222, 222, 222)';
         setTimeout(function(){
                 btnLanzar.disabled = false;
+                btnLanzar.style.backgroundColor=colorStyleLanzar;
+                btnReiniciar.disabled = false;
+                btnReiniciar.style.backgroundColor=colorStyleReiniciar;
         },3000);
 }
 
 btnReiniciar.addEventListener('click',() => {
         monedas.style.animation ='none';
-        tiradaCara=0; 
-        tiradaCruz=0;
+        iniciarVbles();
         mostrarTirada();
 });
